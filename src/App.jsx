958eddda058c983a5e0d2528c2a023d2f250a3e7
hbandi93 <hbandi93@gmail.com>
2022-02-16 21:19:04 +0100

    Show images on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,13 @@
 import axios from "axios";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
+import Gallery from "./components/Gallery";
 
 
 function App() {
-
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects')
-    .then(res => {
-      console.log('this is ', res.data)
-      setData(res.data)
-     
-      }).catch(err => console.log(err))
-  }, [])
-
-  const arr = data.map((data, index) => {
-    return (
-      <tr>
-    <td>{data.objectIDs}</td>
-    <td>{data.total}</td>
-    <td>Germany</td>
-  </tr>
-    )
-  })
-
   return (
     <div className="App">
       <h1>This is our art gallery</h1>
-      <table>
-  <tr>
-    <th>ID </th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  {arr}
-  
-</table>
+      <Gallery />
     </div>
   );
 }
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,27 +1,37 @@
+import React, { Component } from 'react'
 import axios from 'axios';
-import { useEffect } from 'react';
 
+class Gallery extends Component {
+    state = {
+        page: 1,
+        src: [],
+        ind: 0
+    }
+    async componentDidMount() {
+        let source = []
+        let i = 0
+        while (source.length < 11) {
+            i = i + 1
+            await axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + i)
+                .then(res => {
+                    console.log(res.data.isPublicDomain);
+                    if (res.data.isPublicDomain) {
+                        source.push(res.data.primaryImageSmall)
+                    }
+                    else { return }
+                })
+                .catch(err => { return })
+        }
+        this.setState({ src: source, ind: i })
+    }
+    render() {
+        return (
+            < div >
+                <p>{this.state.page}</p>
+                {this.state.src.map(elem => { return <img src={elem} alt="Kép" /> })}
+            </div >
+        );
+    }
+}
 
-
-
-
-const api = axios.create({
-    baseURL: "https://collectionapi.metmuseum.org/public/collection/v1/objects"
-})
-
-
-    axios.get( 'https://collectionapi.metmuseum.org/public/collection/v1/objects')
-    .then((response) => {
-        console.log(response)
-        const myGalery = response.data;
-        
-    } )
-    .catch ((error) => {
-        console.log(error)
-        });
-
-
-
-
-
-  
+export default Gallery;
