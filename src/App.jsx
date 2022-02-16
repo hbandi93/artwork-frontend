b68a48957e3c542255dadc5f338d87a1eafeed65
hbandi93 <hbandi93@gmail.com>
2022-02-16 11:50:29 +0100

    Do the basic homepage with some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import Home from "./components/Home";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li><Link style={{ textDecoration: 'none' }} to="/">Home</Link></li>
+              <li>Gallery</li>
+              <li>Profile</li>
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+        <footer>Ez a footer</footer>
+      </div>
+    </Router>
   );
 }
 
