5bdedcbdbc4b398807f6566d1bdf6cadc91fc8c4
uncasa <uncasaa@gmail.com>
2022-02-16 11:37:49 +0100

    Gallery api add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
-import Login from "./components/Login";
+import axios from "axios";
+import { useEffect,useState } from "react";
+
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects')
+    .then(res => {
+      console.log('this is ', res.data)
+      setData(res.data)
+     
+      }).catch(err => console.log(err))
+  }, [])
+
+  const arr = data.map((data, index) => {
+    return (
+      <tr>
+    <td>{data.objectIDs}</td>
+    <td>{data.total}</td>
+    <td>Germany</td>
+  </tr>
+    )
+  })
+
   return (
     <div className="App">
-      <Login />
+      <h1>This is our art gallery</h1>
+      <table>
+  <tr>
+    <th>ID </th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  {arr}
+  
+</table>
     </div>
   );
 }
