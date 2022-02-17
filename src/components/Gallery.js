2690a03891f78aa67c29b61ab5ce75972091136d
hbandi93 <hbandi93@gmail.com>
2022-02-17 23:35:05 +0100

    Refactor gallery to use hooks and add pagination

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,42 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import axios from 'axios';
 
-class Gallery extends Component {
-    state = {
-        page: 1,
-        src: [],
-        ind: 0
-    }
-    async componentDidMount() {
-        let source = []
-        let i = 33
-        while (source.length < 11) {
-            i = i + 1
+const Gallery = () => {
+    const [page, setPage] = useState(1)
+    const [source, setSource] = useState([])
+    const [index, setIndex] = useState(34)
+
+    const fetchImages = async () => {
+        let data = []
+        let i = index
+        while (data.length < 1) {
             await axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + i)
                 .then(res => {
                     console.log(res.data.isPublicDomain);
                     if (res.data.isPublicDomain) {
-                        source.push(res.data)
+                        data.push(res.data)
                     }
                     else { return }
                 })
                 .catch(err => { return })
+            i = i + 1
         }
-        this.setState({ src: source, ind: i })
+        setSource(data)
+        setIndex(i)
     }
-    render() {
-        return (
-            < div >
-                <h1>This is our gallery.</h1>
-                {this.state.src.map(elem => {
-                    return <div className='imgContainer'>
-                        <h2>{elem.objectName}</h2>
-                        <img src={elem.primaryImageSmall} alt="Kép" />
-                    </div>
-                })}
-            </div >
-        );
+
+    const previousPage = () => {
+        if (page >= 1) {
+            setIndex(index - 1)
+            setPage(page - 1)
+        }
     }
+
+    const nextPage = () => {
+        setPage(page + 1)
+    }
+
+    useEffect(() => {
+        fetchImages()
+        console.log("source: ", source, "index: ", index, "page: ", page);
+    }, [page])
+    return (
+        < div key={page}>
+            <h1>This is our gallery.</h1>
+            {source.map(elem => {
+                return <div className='imgContainer' key={elem.accessionNumber}>
+                    <h2>{elem.objectName}</h2>
+                    <img src={elem.primaryImageSmall} alt="Kép" />
+                </div>
+            })}
+            <button onClick={previousPage} disabled={page === 1}>Previous page</button>
+            <button onClick={nextPage}>Next page</button>
+        </div >
+    );
 }
 
 export default Gallery;
