bf4675b5d80d813ff72d8a789eafbeb1a0442028
hbandi93 <hbandi93@gmail.com>
2022-02-19 14:51:37 +0100

    Make search work

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,26 +2,28 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import ImageClicker from './ImageClicker';
 
-const Gallery = ({ objectID }) => {
+const Gallery = ({ objectID, browse }) => {
     const [page, setPage] = useState(1)
     const [source, setSource] = useState([])
     const [index, setIndex] = useState(0)
     const [search, setSearch] = useState("")
     const displayNumberOfImages = 20
 
-    const browse = () => {
-
-    }
-
     const fetchImages = async () => {
+
+        if (!objectID) return
+
         let data = []
         let i = index
-        while (data.length < displayNumberOfImages) {
-            await axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + objectID[i])
-                .then(res => {
-                    data.push(res.data)
-                })
-                .catch(err => { return })
+
+        while (data.length < displayNumberOfImages && data.length !== objectID.length) {
+            if (objectID[i]) {
+                await axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + objectID[i])
+                    .then(res => {
+                        data.push(res.data)
+                    })
+                    .catch(err => { return })
+            }
             i = i + 1
         }
         setSource(data)
@@ -44,12 +46,13 @@ const Gallery = ({ objectID }) => {
     }, [page])
 
     return (
-        < div key={page}>
+        < div key={`${page}`}>
             <h1>This is our gallery.</h1>
             <label>Search:
                 <input type="text" onChange={(e) => setSearch(e.target.value)} value={search} />
-                <button onClick={browse}>Search</button>
+                <button onClick={() => browse(search)}>Search</button>
             </label>
+            {!objectID || objectID.length === 0 ? <div>No result found</div> : ""}
             {source.map(elem => <ImageClicker elem={elem} key={elem.accessionNumber} />)}
             <button onClick={previousPage} disabled={page === 1}>Previous page</button>
             <button onClick={nextPage}>Next page</button>
