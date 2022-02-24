bc412dc0bc9110803d47db9ffb363851a18954ba
hbandi93 <hbandi93@gmail.com>
2022-02-24 10:28:39 +0100

    Update Backend reach

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 class SignUp extends Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
-            fullName:'',
-            username:'',
-            email:'',
-            password:''
+            fullName: '',
+            username: '',
+            email: '',
+            password: ''
 
         }
         this.changeFullName = this.changeFullName.bind(this)
@@ -18,72 +18,72 @@ class SignUp extends Component {
         this.onSubmit = this.onSubmit.bind(this)
     }
 
-    changeFullName(event){
+    changeFullName(event) {
         this.setState({
-            fullName:event.target.value
+            fullName: event.target.value
         })
     }
-    changeUsername(event){
+    changeUsername(event) {
         this.setState({
-            username:event.target.value
-        })
-    }
-
-    changeEmail(event){
-        this.setState({
-            email:event.target.value
+            username: event.target.value
         })
     }
 
-    changePassword(event){
+    changeEmail(event) {
         this.setState({
-            password:event.target.value
+            email: event.target.value
         })
     }
-    onSubmit(event){
+
+    changePassword(event) {
+        this.setState({
+            password: event.target.value
+        })
+    }
+    onSubmit(event) {
         event.preventDefault()
         const registered = {
             fullName: this.state.fullName,
             username: this.state.username,
-            email:this.state.email,
+            email: this.state.email,
             password: this.state.password
         }
 
-        axios.post('http://localhost:4000/signup',registered)
+        axios.post('http://18.157.53.210:4000/signup', registered)
             .then(response => console.log(response.data))
     }
     render() {
-        return(
+        return (
             <div>
                 <div className='container'>
                     <div className='form-div'>
                         <form onSubmit={this.onSubmit}>
                             <input type='text'
-                            placeholder='Full name'
-                            onChange={this.changeFullName}
-                            value={this.state.fullName}
-                            className='signup-form-fields'
+                                placeholder='Full name'
+                                onChange={this.changeFullName}
+                                value={this.state.fullName}
+                                className='signup-form-fields'
                             />
 
-                            <input type="text" 
-                            placeholder='Username'
-                            onChange={this.changeUsername}
-                            value={this.state.username}
-                            className='signup-form-fields'
+                            <input type="text"
+                                placeholder='Username'
+                                onChange={this.changeUsername}
+                                value={this.state.username}
+                                className='signup-form-fields'
                             />
 
-                            <input type="text" 
-                            placeholder='E-mail'
-                            onChange={this.changeEmail}
-                            value={this.state.email}
-                            className='signup-form-fields'
+                            <input type="text"
+                                placeholder='E-mail'
+                                onChange={this.changeEmail}
+                                value={this.state.email}
+                                className='signup-form-fields'
                             />
 
-                            <input type="password" 
-                            placeholder='Password'
-                            onChange={this.changePassword}
-                            value={this.state.password}
-                            className='signup-form-fields'
+                            <input type="password"
+                                placeholder='Password'
+                                onChange={this.changePassword}
+                                value={this.state.password}
+                                className='signup-form-fields'
                             />
 
                             <input type="submit" className='signup-submit-btn' value='Submit' />
@@ -94,7 +94,7 @@ class SignUp extends Component {
 
             </div>
         );
-    
+
     }
 }
 export default SignUp;
